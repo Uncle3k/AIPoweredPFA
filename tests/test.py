439e805e5d1fb208012e748e2fd391da3f0416c4
Uncle3k <shaheer11212002@gmail.com>
2024-12-19 05:52:05 -0500

    .gitignore fix

---
+++ tests/test.py
@@ -0,0 +1,12 @@
+root/
+├── src/
+│   ├── __pycache__/
+│   ├── Dockerfile
+│   ├── main.py
+│   ├── requirements.txt
+├── tests/ 
+│   ├── test_example.py    
+├── .gitignore
+├── .env
+├── README.md
+├── docker-compose.yml 
